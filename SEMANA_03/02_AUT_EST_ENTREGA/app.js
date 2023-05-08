21241543780b0aa32c389f3fa883aba3371f1eb3
Daniel-Zular <daniel.zular@sou.inteli.edu.br>
2023-05-08 17:20:51 -0300

    testandopond3

--- SEMANA_03/02_AUT_EST_ENTREGA/app.js
+++ SEMANA_03/02_AUT_EST_ENTREGA/app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3030;
+const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
-const DBPATH = 'curriculo.db'; //use o nome que você achar melhor para o banco de dados
+const DBPATH = 'database.db'; //use o nome que você achar melhor para o banco de dados
+
 
 app.use(express.json());
-app.get('/formacao', (req, res) => {
+
+// READ
+app.get('/user', (req, res) => {
+    let user_key = req.query.user_key;
     res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*'); 
     var db = new sqlite3.Database(DBPATH); // Abre o banco
-    var sql = 'SELECT título AS "curso" FROM formação';
+    var sql = `SELECT * FROM user WHERE user_key = ${user_key}`;
     db.all(sql, [],  (err, rows ) => {
         if (err) {
             throw err;
@@ -20,6 +24,94 @@ app.get('/formacao', (req, res) => {
     });
     db.close(); // Fecha o banco
 });
+
+app.get('/formacao', (req, res) => {
+    let user_key = req.query.user_key;
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = `SELECT * FROM formacao WHERE user_key = ${user_key}`;
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+//CREATE
+
+app.post('/create-user', (req, res) => {
+    let nome = req.body.nome;
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = `INSERT INTO user (nome) VALUES ( "${nome}")`;
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+app.post('/create-formacao', (req, res) => {
+    let titulo = req.body.titulo;
+    let instituicao = req.body.instituicao
+    let user_key = req.body.user_key
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = `INSERT INTO formacao (titulo, instituicao, user_key) VALUES ('${titulo}','${instituicao}',${user_key})`;
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+//UPDATE
+
+app.put('/update-formacao', (req, res) => {
+    let titulo = req.body.titulo;
+    let instituicao = req.body.instituicao
+    let user_key = req.body.user_key
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = `UPDATE formacao SET titulo = '${titulo}', instituicao = '${instituicao}' WHERE user_key = ${user_key}`;
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+//DELETE
+
+app.delete('/delete-formacao-pelo-titulo', (req, res) => {
+    let titulo = req.body.titulo;
+    let instituicao = req.body.instituicao
+    let user_key = req.body.user_key
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    var sql = `DELETE FROM formacao WHERE titulo = "${titulo}"`;
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
 
 /* Inicia o servidor */
 app.listen(port, hostname, () => {
